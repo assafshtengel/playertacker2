53934d4a963f764de802deebb3fa8efc2127b1fd
assafshtengel <socr.co.il@gmail.com>
2024-12-15 00:32:10 +0200

    Create PlayerDashboard.js

---
+++ frontend/src/PlayerDashboard.js
@@ -0,0 +1,13 @@
+// frontend/src/pages/PlayerDashboard.js
+import React from 'react';
+
+function PlayerDashboard(){
+  return (
+    <div style={{padding:'20px'}}>
+      <h1>מסך שחקן</h1>
+      <p>כאן בהמשך יוצגו סטטיסטיקות אישיות, מטרות, וסיכומי ביצועים.</p>
+    </div>
+  );
+}
+
+export default PlayerDashboard;
