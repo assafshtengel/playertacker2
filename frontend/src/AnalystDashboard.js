a98fc8c115cbdb5f209f76bead23428572a51ae7
assafshtengel <socr.co.il@gmail.com>
2024-12-15 00:32:51 +0200

    Create  AnalystDashboard.js

---
+++ frontend/src/AnalystDashboard.js
@@ -0,0 +1,13 @@
+// frontend/src/pages/AnalystDashboard.js
+import React from 'react';
+
+function AnalystDashboard(){
+  return (
+    <div style={{padding:'20px'}}>
+      <h1>מסך אנליסט</h1>
+      <p>כאן בעתיד נציג כלים לניתוח סטטיסטיקות, הפקת דוחות, השוואות בין משחקים ועוד.</p>
+    </div>
+  );
+}
+
+export default AnalystDashboard;
